e0ed273918871576ff87ce968d54b922f80edc7c
Zhixi <joyx29580@gmail.com>
2023-11-27 15:01:49 -0800

    Implemented terminal based version with image classification and providing recipes.

---
+++ src/main.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+import classification_model
+import recipes
+
+def main():
+    parser = argparse.ArgumentParser(description="This scrip processes images and return classification results")
+
+    parser.add_argument('image_path', type=str, help='Path to the image file')
+    args = parser.parse_args()
+    
+    # Error handling
+    image_path = valid_path(args.image_path)
+
+    # Classify image
+    predicted_label = classification_model.image_classify(image_path)
+
+    print(predicted_label)
+    recipes.print_recipes(1, predicted_label)
+
+
+def valid_path(image_path):
+    if not os.path.isfile(image_path):
+        raise argparse.ArgumentTypeError(f"The file {image_path} does not exist.")
+    if not image_path.lower().endswith(('.png', '.jpeg','.jpg')):
+        raise argparse.ArgumentTypeError("File must be a PNG or JPG imgae.")
+    return image_path
+    
+
+if __name__ == "__main__":
+    main()
+
+"""
+print("#######################################")
+print("# Welcome to the Green Gourment Guide #")
+print("#######################################")
+"""
---
+++ src/recipes.py
@@ -0,0 +1,93 @@
+"""This module integretes the TheMealDB API and implements the feature of recipe processing."""
+
+#######################
+# Recipes Processing #
+#######################
+
+import requests
+import random
+
+def get_recipes_by_ingredient(ingredient: str):
+    """ 
+        Returns a dictionary of recipes based on provided ingredient. 
+        Returns None if the request failed.
+    """
+    url = f"https://www.themealdb.com/api/json/v1/1/filter.php?i={ingredient}"
+    response = requests.get(url)
+    #  HTTP status code that the server returns, 200 is the HTTP status code for "OK," 
+    #  which means the request was successful, and the server has sent back a valid response.
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+def get_recipes_by_id(id: str):
+    url = f"https://www.themealdb.com/api/json/v1/1/lookup.php?i={id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+def get_fixed_num_recipe_ids(num: int, recipes: dict) -> list:
+    """
+    Returns a list of random recipe ids from recipes dictionary. 
+    If the number of meals in recipes are less than num, return a list of all meal ids.
+    Returns None if no meal exists in recipes.
+    
+    Args:
+        num: The number of recipe ids to generate.
+        recipes: The recipes dictionary
+
+    Example output:
+    ['52815', '52811', '52775']
+    """
+    meal_ls = recipes["meals"]
+    # No meal exists in recipes
+    if not meal_ls:
+        raise ValueError("No meal exists in recipes.")
+
+    total_meal_num = len(meal_ls)
+    # When meal numbers are less than num
+    if total_meal_num < num:
+        random_nums = range(total_meal_num)
+    else:
+        random_nums = random.sample(range(total_meal_num), num)
+
+    random_ids = []
+    for num in random_nums:
+        random_ids.append(recipes["meals"][num]["idMeal"])
+
+    return random_ids
+
+
+def get_recipe_name(id: str) -> str:
+    """ Returns the meal name based on the meal id. """
+    valid_id(id)
+    return get_recipes_by_id(id)["meals"][0]["strMeal"]
+
+
+def get_recipe_instruction(id: str) -> str:
+    """ Returns the meal instruction based on the meal id."""
+    valid_id(id)
+    return get_recipes_by_id(id)["meals"][0]["strInstructions"]
+
+
+def valid_id(id: str):
+    """ Raise an error when the id does not exist. """
+    if not get_recipes_by_id(id)["meals"]:
+        raise ValueError("The id does not exist.")
+
+
+def print_recipes(num, label):
+    recipes = get_recipes_by_ingredient(label)
+    id_ls = get_fixed_num_recipe_ids(num, recipes)
+    for id in id_ls:
+        print(get_recipe_name(id))
+        print(get_recipe_instruction(id))
+        print("\n")
+
+
+
